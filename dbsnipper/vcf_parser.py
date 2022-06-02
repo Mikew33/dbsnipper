cdd8ad5ddb86fed77757e6cae0d43516d56f486e
linyc74 <yclin.python@gmail.com>
2022-06-02 16:38:58 +0800

    rewrite parser.next()

--- dbsnipper/vcf_parser.py
+++ dbsnipper/vcf_parser.py
@@ -3,15 +3,12 @@ from typing import IO
 
 class VcfParser:
 
-    context: str
-    temporary: str
     header: str
     fh: IO
 
     def __init__(self, vcf: str):
         self.fh = open(vcf, 'r')
         self.set_header()
-        self.next()
 
     def set_header(self):
         num_header_lines = 0
@@ -30,26 +27,8 @@ class VcfParser:
                 break
         self.header = ''.join(temp)
 
-    def next(self):
-        self.fh.seek(0)
-
-        count = 0
-        temp1 = []
-        for line in self.fh:
-            if count == 1:
-                break
-            if line.startswith('#'):
-                continue
-            else:
-                temp1.append(line)
-                count += 1
-        self.temporary = ''.join(temp1)
-
-        temp2 = []
-        word_count = 0
-        for w in self.temporary:
-            word_count += 1
-            temp2.append(w)
-            if word_count == 51:
-                break
-        self.context = ''.join(temp2)
+    def next(self) -> str:
+        line = self.fh.readline()
+        line.split('\t')
+        ret = ''
+        return ret
--- test/test_vcf_parser.py
+++ test/test_vcf_parser.py
@@ -1,6 +1,8 @@
 from dbsnipper.vcf_parser import VcfParser
 from .setup import TestCase
 
+import pandas as pd
+
 
 class TestVcfParser(TestCase):
 
@@ -22,5 +24,15 @@ class TestVcfParser(TestCase):
 
     def test_next(self):
         parser = VcfParser(vcf=f'{self.indir}/tiny.vcf')
+
+        actual = parser.next()
         expected = f'1\t101\trs6054257\tG\tA\t29\tPASS\tNS=3;DP=14;AF=0.5;DB;H2'
-        self.assertEqual(expected, parser.context)
+        self.assertEqual(expected, actual)
+
+        # actual = parser.next()
+        # expected = f'2\t102\t.\tT\tA\t3\tq10\tNS=3;DP=11;AF=0.017'
+        # self.assertEqual(expected, actual)
+
+        # actual = parser.next()
+        # expected = '3\t103\trs6040355\tA\tG,T\t67\tPASS\tNS=2;DP=10;AF=0.333,0.667;AA=T;DB'
+        # self.assertEqual(expected, actual)
